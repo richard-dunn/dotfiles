35edf44cceb41450f81261c6cb08dbaa0a1b4d30
Richard Dunn <hello@richard.photos>
2024-08-13 16:28:43 +0100

    add install script for arch

---
+++ scripts/arch_install.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# git will be installed before running this and dotfiles will be downloaded to ~/dotfiles/
+# this script will be in ~/dotfiles/scripts/
+
+# Ensure script is run as a non-root user
+if [ "$EUID" -eq 0 ]; then
+  echo "Please do not run as root"
+  exit
+fi
+
+# Update the system
+sudo pacman -Syu --noconfirm
+
+# Make sure basic utilities are installed
+sudo pacman -S --noconfirm base-devel openssl libffi zlib
+
+# Install timeshift
+sudo pacman -S --noconfirm timeshift
+
+# Record initial snapshot with timeshift
+if command -v timeshift &> /dev/null
+then
+  sudo timeshift --create --comments "Initial post-install snapshot"
+fi
+
+# Install drivers for Thinkpad T480
+sudo pacman -S --noconfirm intel-ucode xf86-video-intel iwlwifi firmware sof-firmware acpi acpi_call tlp
+
+# Enable tlp for power management
+sudo systemctl enable tlp
+sudo systemctl start tlp
+
+# Change shell to zsh and symlink .zshrc
+chsh -s /bin/zsh
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Install tmux and symlink conf files
+sudo pacman -S --noconfirm tmux
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# Install nvim and symlink conf files
+sudo pacman -S --noconfirm neovim
+ln -sf ~/dotfiles/.config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/dotfiles/.config/nvim/lua ~/.config/nvim/lua
+
+# Install wayland, hyprland, hyprpaper, waybar and symlink conf files
+sudo pacman -S --noconfim wayland xorg-server-xwayland wayland-protocols hyprland-git hyprpaper waybar
+ln -sf ~/dotfiles/.config/hypr ~/.config/hypr
+ln -sf ~/dotfiles/.config/wayland ~/.config/wayland
+
+# Install kitty and symlink conf files
+sudo pacman -S --noconfirm kitty
+ln -sf ~/dotfiles/.config/kitty ~/.config/kitty
+
+# Install wofi
+sudo pacman -S --noconfirm wofi
+ln -sf ~/dotfiles/.config/wofi ~/.config/wofi
+
+# Install firefox 
+sudo pacman -S --noconfirm firefox
+
+# Install bluetooth utils
+sudo pacman -S --noconfirm bluez bluez-utils pulseaudio-bluetooth
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
+
+# Install docker
+sudo pacman -S --noconfirm docker
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+
+# Install nodejs
+sudo pacman -S --noconfirm nodejs npm
+
+# Install rbenv and ruby-build
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+cd ~/.rbenv && src/configure && make -C src
+source ~/.zshrc
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# Install ruby and bundler
+rbenv install 3.1.2
+rbenv global 3.1.2
+gem install bundler
+rbenv rehash
+
+echo "Installation and configuration complete! Please reboot for all changes to take effect."
